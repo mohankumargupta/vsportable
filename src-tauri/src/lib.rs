925aea827202bb3f5a686b6203acf15a32354123
Raspberry Pi <raspberrypisig@gmail.com>
2024-10-14 00:38:47 +1100

    broken rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,13 @@
-use std::{fs, io, path::PathBuf};
-
+use futures_util::TryStreamExt;
+use reqwest;
+use std::{
+    error::Error,
+    fs::{self},
+    io::{self, Write},
+    path::PathBuf,
+};
+use tokio::fs::File;
+use tokio::io::{AsyncWriteExt, BufWriter};
 /*
 fn list_folders(path: PathBuf) -> Result<Vec<String>, io::Error> {
     fs::read_dir(path)?
@@ -75,10 +83,69 @@ fn folder_exists(folder: String) -> bool {
     dirs::download_dir().map_or(false, |download_dir| download_dir.join(folder).exists())
 }
 
+#[derive(Debug)]
+struct HttpError {
+    status_code: u16,
+    response_body: String,
+}
+
+impl std::fmt::Display for HttpError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "HTTP error: status code {}: {}",
+            self.status_code, self.response_body
+        )
+    }
+}
+
+impl Error for HttpError {}
+
+//on_progress: Channel<ProgressPayload>,
+
+async fn download(url: &str, file_path: &PathBuf) -> Result<(), Box<dyn Error>> {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(url)
+        .send()
+        .await?
+        .error_for_status()
+        .map_err(|err| HttpError {
+            status_code: err.status().unwrap_or_default().as_u16(),
+            response_body: err.to_string(),
+        })?;
+
+    let total = response.content_length().unwrap_or(0);
+
+    let mut file = BufWriter::new(File::create(file_path).await?);
+    let mut stream = response.bytes_stream();
+
+    let mut downloaded_bytes = 0;
+    while let Some(chunk) = stream.try_next().await? {
+        file.write_all(&chunk).await?;
+        downloaded_bytes += chunk.len() as u64;
+        /*
+        on_progress.send(ProgressPayload {
+            progress: downloaded_bytes,
+            total,
+            percentage: (downloaded_bytes as f64 / total as f64) * 100.0,
+        }).await?;
+        */
+    }
+
+    file.flush().await?;
+
+    Ok(())
+}
+
 #[tauri::command]
 fn vsinstall(folder: String) -> bool {
     let newfolder = dirs::download_dir().unwrap().join(folder);
     let result = fs::create_dir(newfolder.clone()).is_ok();
+    let vscode_zip = newfolder.join("vscode.zip");
+    let url = "https://update.code.visualstudio.com/latest/win32-x64-archive/stable";
+    download(url, &vscode_zip);
     //println!("{:?} - {}", newfolder, result);
     result
 }
