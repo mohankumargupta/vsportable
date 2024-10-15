ceb24011ea2bd60caaca610679b5997abf151ad6
Raspberry Pi <raspberrypisig@gmail.com>
2024-10-15 12:19:41 +1100

    untested

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
-use async_compression::tokio::bufread::DeflateDecoder;
+//use async_compression::tokio::bufread::DeflateDecoder;
 use async_zip::tokio::read::seek::ZipFileReader;
-use futures_util::TryStreamExt;
+//use futures_util::TryStreamExt;
+use futures_util::StreamExt;
 use reqwest;
 use serde::Serialize;
 use std::{
@@ -8,8 +9,12 @@ use std::{
     path::PathBuf,
 };
 use thiserror::Error;
-use tokio::fs::File;
-use tokio::io::{AsyncWriteExt, BufReader, BufWriter};
+use tokio::{
+    fs::{copy, create_dir_all, remove_file, File, OpenOptions},
+    io::{AsyncWriteExt, BufReader, BufWriter},
+};
+use tokio_util::compat::TokioAsyncReadCompatExt;
+use tokio_util::compat::TokioAsyncWriteCompatExt;
 
 mod vsinstall;
 use vsinstall::Error::ReqwestError;
@@ -153,7 +158,8 @@ async fn download(url: &str, file_path: &PathBuf) -> Result<(), vsinstall::Error
     let mut stream = response.bytes_stream();
 
     let mut downloaded_bytes = 0;
-    while let Some(chunk) = stream.try_next().await? {
+    while let Some(chunk_result) = stream.next().await {
+        let chunk = chunk_result.map_err(ReqwestError)?;
         file.write_all(&chunk).await?;
         downloaded_bytes += chunk.len() as u64;
         /*
@@ -169,6 +175,63 @@ async fn download(url: &str, file_path: &PathBuf) -> Result<(), vsinstall::Error
 
     Ok(())
 }
+
+/*
+async fn unpack_zip(source_path: &PathBuf, out_dir: &Path) -> Result<(), anyhow::Error> {
+    let archive_file = File::open(source_path).await?;
+    let archive = BufReader::new(archive_file).compat();
+    let mut reader = ZipFileReader::new(archive)
+        .await
+        .expect("Failed to read zip file");
+    for index in 0..reader.file().entries().len() {
+        let entry = reader.file().entries().get(index).unwrap();
+        let path = out_dir.join(sanitize_file_path(entry.filename().as_str().unwrap()));
+        // If the filename of the entry ends with '/', it is treated as a directory.
+        // This is implemented by previous versions of this crate and the Python Standard Library.
+        // https://docs.rs/async_zip/0.0.8/src/async_zip/read/mod.rs.html#63-65
+        // https://github.com/python/cpython/blob/820ef62833bd2d84a141adedd9a05998595d6b6d/Lib/zipfile.py#L528
+        let entry_is_dir = entry.dir().unwrap();
+
+        let mut entry_reader = reader
+            .reader_without_entry(index)
+            .await
+            .expect("Failed to read ZipEntry");
+
+        if entry_is_dir {
+            // The directory may have been created if iteration is out of order.
+            if !path.exists() {
+                create_dir_all(&path)
+                    .await
+                    .expect("Failed to create extracted directory");
+            }
+        } else {
+            // Creates parent directories. They may not exist if iteration is out of order
+            // or the archive does not contain directory entries.
+            let parent = path
+                .parent()
+                .expect("A file entry should have parent directories");
+            if !parent.is_dir() {
+                create_dir_all(parent)
+                    .await
+                    .expect("Failed to create parent directories");
+            }
+            let writer = OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(&path)
+                .await
+                .expect("Failed to create extracted file");
+            futures_lite::io::copy(&mut entry_reader, &mut writer.compat_write())
+                .await
+                .expect("Failed to copy to extracted file");
+
+            // Closes the file and manipulates its metadata here if you wish to preserve its metadata from the archive.
+        }
+    }
+
+    Ok(())
+}
+*/
 
 async fn unzip(zip_file: &PathBuf) -> Result<(), vsinstall::Error> {
     let mut file = BufReader::new(File::open(zip_file).await?);
